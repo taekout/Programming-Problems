8785f6018340ea8dcdcb46d2628ed344d6e616e0
Taekyu <taekout@gmail.com>
2018-09-27 11:46:59 -0700

    PhoneReserve system draft
    PhoneReserve system draft without locks, distributed computing.

--- PhoneReserveSystem.cpp
+++ PhoneReserveSystem.cpp
@@ -1,24 +1,46 @@
 class PhoneNumberGiver
 {
-    unordered_map<string> available;
-    unordered_map<string> reserved;
+    unordered_set<string> available;
+    unordered_set<string> reserved;
     unordered_map<string, customerID> assigned;
+    
+    Reserve(const string & number)
+    {
+        reserved.emplace(str);
+    }
     
 public:
     vector<string> Reserve(int n)
     {
-        
+        vector<string> result;
+        for(size_t i = 0, auto it = available.begin() ; it != available.end() && i < n ; i++, it++)
+        {
+            result.push_back(*it);
+        }
+        for(auto & number : result)
+        {
+            available.erase(number);
+            Reserve(number);
+        }
+        return result;
     }
     
-    Reserve(const string & number)
+    void Assign(const string & number)
     {
+        auto it = reserve.find(number);
+        if(it != reserve.end())
+        {
+            assigned.emplace(number);
+        }
+        else
+            throw std::exception();
     }
     
-    ReturnNumber(const string & number)
+    void ReturnNumber(const vector<string> & number)
     {
-    }
-    
-    Assign(const string & number)
-    {
+        for(auto & s : number)
+        {
+            reserved.erase(s);
+        }
     }
 };
