9696d0c740537a75b2f355396e939f4eeb7a28ce
Taekyu <taekout@gmail.com>
2018-09-27 11:32:36 -0700

    initial commit phone number reserve/assign system.

---
+++ PhoneReserveSystem.cpp
@@ -0,0 +1,24 @@
+class PhoneNumberGiver
+{
+    unordered_map<string> available;
+    unordered_map<string> reserved;
+    unordered_map<string, customerID> assigned;
+    
+public:
+    vector<string> Reserve(int n)
+    {
+        
+    }
+    
+    Reserve(const string & number)
+    {
+    }
+    
+    ReturnNumber(const string & number)
+    {
+    }
+    
+    Assign(const string & number)
+    {
+    }
+};
